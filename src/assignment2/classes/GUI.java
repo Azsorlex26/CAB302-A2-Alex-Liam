681ecd97441fb3756ab2832d8f00e7047249891a
Alex Rozsa <aorozsa@gmail.com>
2018-05-10 13:54:52 +1000

    Implemented Interface into GUI

--- src/assignment2/classes/GUI.java
+++ src/assignment2/classes/GUI.java
@@ -1,11 +1,17 @@
 package assignment2.classes;
 
+import java.awt.Font;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 
 /**
  * The interface for the program
@@ -16,10 +22,22 @@ import javax.swing.JFrame;
 public class GUI extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = -8954008955048531845L;
-	public static final int WIDTH = 640;
-	public static final int HEIGHT = 480;
+	public static final int WIDTH = 953;
+	public static final int HEIGHT = 536;
 	private static Store store;
 	private JButton btnName, btnCapital;
+	private JPanel stockManagementPane;
+	private JTable tblInventory;
+	private JLabel lblNewLabel;
+
+	/**
+	 * Program entry point
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new GUI("UMart");
+	}
 
 	/**
 	 * GUI constructor
@@ -29,15 +47,26 @@ public class GUI extends JFrame implements ActionListener {
 	 */
 	public GUI(String title) throws HeadlessException {
 		super(title);
-		store = Store.makeStore("UMart");
-		setLayout(null); // This allows manual adjustment of the elements.
-							// Without this the buttons take up the whole screen no matter what
+		store = Store.makeStore(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		setSize(WIDTH, HEIGHT);
+		setBounds(100, 100, WIDTH, HEIGHT);
 
-		btnName = newButton("Store name", 0, 0, 100, 45);
-		btnCapital = newButton("Store capital", 0, 50, 120, 30);
+		// Create a tabbedPane and add layouts
+		JTabbedPane storeTabs = new JTabbedPane();
+		storeTabs.setFont(new Font("Sylfaen", Font.PLAIN, 35));
+
+		tblInventory = new JTable();
+
+		stockManagementPane = new JPanel();
+		GroupLayout stockLayout = new GroupLayout(stockManagementPane);
+		stockManagementPane.setLayout(stockLayout);
+
+		storeTabs.addTab("Stock Management", null, stockManagementPane, null);
+		storeTabs.addTab("Inventory", null, tblInventory, null);
+
+		storeTabs.setVisible(true);
+		setVisible(true);
+		add(storeTabs);
 	}
 
 	/**
@@ -51,8 +80,7 @@ public class GUI extends JFrame implements ActionListener {
 	 * @return button
 	 */
 	private JButton newButton(String text, int x, int y, int width, int height) {
-		JButton button = new JButton();
-		button.setText(text);
+		JButton button = new JButton(text);
 		button.setBounds(x, y, width, height);
 		button.addActionListener(this);
 		add(button);
@@ -72,14 +100,5 @@ public class GUI extends JFrame implements ActionListener {
 		} else if (e.getSource() == btnCapital) {
 			System.out.println(store.capital());
 		}
-	}
-
-	/**
-	 * Program entry point
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		new GUI("Inventory Manager Tycoon");
 	}
 }
